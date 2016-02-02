d31bb0f10525c6ebd8b22de008a146797c5a4bcd
HybridDog <ovvv@web.de>
2016-02-02 19:04:11 +0100

    Add a shader setting to increase contrast by distance
    fix spaces, change ints to floats and dont disable tonemapping
    
    pass color.rgb instead of color
    
    convert the subtrahend to a vec3

--- src/defaultsettings.cpp
+++ src/defaultsettings.cpp
@@ -156,6 +156,7 @@ void set_default_settings(Settings *settings)
 	settings->setDefault("texture_clean_transparent", "false");
 	settings->setDefault("texture_min_size", "64");
 	settings->setDefault("tone_mapping", "false");
+	settings->setDefault("far_contrast", "false");
 	settings->setDefault("enable_bumpmapping", "false");
 	settings->setDefault("enable_parallax_occlusion", "false");
 	settings->setDefault("generate_normalmaps", "false");
