a273ebc0c97196b593fae50df738ef600e45bb40
Hybrid Dog <ovvv@web.de>
2018-01-21 17:33:07 +0100

    Make it work

--- src/client/ssim_downscaling.cpp
+++ src/client/ssim_downscaling.cpp
@@ -2,7 +2,7 @@
 
 #include "ssim_downscaling.h"
 
-#include <assert.h>
+#include "log.h"
 
 #define SQR_NP 2 // squareroot of the patch size, recommended: 2
 
@@ -55,12 +55,11 @@ static void ycbcr2rgb(float y, float cb, float cr, u8 *r, u8 *g, u8 *b)
  */
 static struct matrix *image_to_matrices(u32 *raw, u32 w, u32 h)
 {
-	struct matrix *matrices =
-		(struct matrix *)malloc(4 * sizeof(struct matrix));
+	struct matrix *matrices = new struct matrix[4];
 	for (int i = 0; i < 4; ++i) {
 		matrices[i].w = w;
 		matrices[i].h = h;
-		matrices[i].data = (float *)malloc(w * h * sizeof(float));
+		matrices[i].data = new float[w * h];
 	}
 	for (u32 i = 0; i < w * h; ++i) {
 		u8 *rgba = (u8 *)&raw[i];
@@ -72,6 +71,186 @@ static struct matrix *image_to_matrices(u32 *raw, u32 w, u32 h)
 	}
 	return matrices;
 }
+
+/*! \brief Convert 4 matrices to an rgba image, which is passed
+ */
+static void matrices_to_image(struct matrix *matrices, u32 *raw)
+{
+	int w = matrices[0].w;
+	int h = matrices[0].h;
+	for (int i = 0; i < w * h; ++i) {
+		u8 *rgba = (u8 *)&raw[i];
+		ycbcr2rgb(matrices[0].data[i], matrices[1].data[i], matrices[2].data[i],
+			rgba, rgba+1, rgba+2);
+		float a = matrices[3].data[i] * 255;
+		*(rgba+3) = CLAMP(a, 0, 255);
+	}
+}
+
+/*! \brief Frees 4 matrices
+ *
+ * \param mat The 4 matrices, e.g. obtained form image_to_matrices.
+ */
+static void free_matrices(struct matrix *matrices)
+{
+	for (int i = 0; i < 4; ++i)
+		delete[] matrices[i].data;
+	delete[] matrices;
+}
+
+/*! \brief The actual downscaling algorithm
+ *
+ * \param mat One of the 4 matrices obtained form image_to_matrices.
+ * \param s The factor by which the image should become downscaled.
+ */
+static void downscale_perc(struct matrix *mat, int s, struct matrix *target)
+{
+	// preparation
+	int w = mat->w; // input width
+	int h = mat->h;
+	float *input = mat->data;
+	int w2 = target->w; // output width
+	int h2 = target->h;
+	int output_elems_cnt = w2 * h2;
+	float *l = new float[output_elems_cnt];
+	float *l2 = new float[output_elems_cnt];
+	float *d = target->data;
+
+	// set d's entries to 0 (because it's used for a sum)
+	for (int i = 0; i < w2 * h2; ++i)
+		d[i] = 0;
+
+	// get l and l2, the input image and it's size are used only here
+	for (int ysm = 0; ysm < h2; ++ysm) {
+		for (int xsm = 0; xsm < w2; ++xsm) {
+			// xsm and ysm are coords for the subsampled image
+			int x = xsm * s;
+			int y = ysm * s;
+			float acc = 0;
+			float acc2 = 0;
+			for (int yc = y; yc < y + s; ++yc) {
+				for (int xc = x; xc < x + s; ++xc) {
+					float v = input[((yc + h) % h) * w + (xc + w) % w];
+					acc += v;
+					acc2 += v * v;
+				}
+			}
+			int ism = ysm*w2+xsm;
+			float divider = 1.0f / (s * s);
+			l[ism] = acc * divider;
+			l2[ism] = acc2 * divider;
+		}
+	}
+
+	float patch_sz_div = 1.0f / (SQR_NP * SQR_NP);
+	// calculate the average of the results of all possible patch sets
+	for (int y_offset = 0; y_offset > -SQR_NP; --y_offset) {
+		for (int x_offset = 0; x_offset > -SQR_NP; --x_offset) {
+			float *m = new float[output_elems_cnt];
+			float *r = new float[output_elems_cnt];
+
+			// get m
+			for (int y = 0; y < h2; ++y) {
+				for (int x = 0; x < w2; ++x) {
+					float acc = 0;
+					// ys (y start) can be -1, then h2-1 is used for the index
+					int ys = y - (y + SQR_NP + y_offset) % SQR_NP;
+					int xs = x - (x + SQR_NP + x_offset) % SQR_NP;
+					for (int yc = ys; yc < ys + SQR_NP; ++yc) {
+						for (int xc = xs; xc < xs + SQR_NP; ++xc) {
+							acc += l[((yc + h2) % h2) * w2 + (xc + w2) % w2];
+						}
+					}
+					m[y*w2+x] = acc * patch_sz_div;
+				}
+			}
+
+			// get r
+			for (int y = 0; y < h2; ++y) {
+				for (int x = 0; x < w2; ++x) {
+					float acc = 0;
+					float acc2 = 0;
+					int ys = y - (y + SQR_NP + y_offset) % SQR_NP;
+					int xs = x - (x + SQR_NP + x_offset) % SQR_NP;
+					for (int yc = ys; yc < ys + SQR_NP; ++yc) {
+						for (int xc = xs; xc < xs + SQR_NP; ++xc) {
+							int i = ((yc + h2) % h2) * w2 + (xc + w2) % w2;
+							acc += l[i] * l[i];
+							acc2 += l2[i];
+						}
+					}
+					int i = y*w2+x;
+					float mv = m[i];
+					float slv = acc * patch_sz_div - mv * mv;
+					float shv = acc2 * patch_sz_div - mv * mv;
+					if (slv >= 0.000001f) // epsilon is 10⁻⁶
+						r[i] = sqrtf(shv / slv);
+					else
+						r[i] = 0;
+				}
+			}
+
+			// get d, which is the output
+			for (int y = 0; y < h2; ++y) {
+				for (int x = 0; x < w2; ++x) {
+					float acc_m = 0;
+					float acc_r = 0;
+					float acc_t = 0;
+					int ys = y - (y + SQR_NP + y_offset) % SQR_NP;
+					int xs = x - (x + SQR_NP + x_offset) % SQR_NP;
+					for (int yc = ys; yc < ys + SQR_NP; ++yc) {
+						for (int xc = xs; xc < xs + SQR_NP; ++xc) {
+							int i = ((yc + h2) % h2) * w2 + (xc + w2) % w2;
+							acc_m += m[i];
+							acc_r += r[i];
+							acc_t += r[i] * m[i];
+						}
+					}
+					int i = y*w2+x;
+					d[i] += (
+							acc_m * patch_sz_div
+							+ acc_r * patch_sz_div * l[i]
+							- acc_t * patch_sz_div
+						);
+				}
+			}
+			delete[] m;
+			delete[] r;
+		}
+	}
+
+	// divide values in d for the (arithmetic) average
+	for (int i = 0; i < w2 * h2; ++i)
+		d[i] *= patch_sz_div;
+
+	// tidy up
+	delete[] l;
+	delete[] l2;
+}
+
+/*! \brief Function which calls functions for downscaling
+ *
+ * \param matrices The content from the original image.
+ * \param downscale_factor Must be a natural number.
+ * \param raw The place where the downscaled srgb image is saved to.
+ */
+void downscale_an_image(struct matrix *matrices, int downscale_factor, u32 *raw)
+{
+	int h = matrices[0].h;
+	int w = matrices[0].w;
+	int h2 = h / downscale_factor;
+	int w2 = w / downscale_factor;
+	struct matrix *smaller_matrices = new struct matrix[4];
+	for (int i = 0; i < 4; ++i) {
+		smaller_matrices[i].h = h2;
+		smaller_matrices[i].w = w2;
+		smaller_matrices[i].data = new float[w2 * h2];
+		downscale_perc(&matrices[i], downscale_factor, &smaller_matrices[i]);
+	}
+	matrices_to_image(smaller_matrices, raw);
+	free_matrices(smaller_matrices);
+}
+
 
 
 video::ITexture *add_texture_with_mipmaps(const std::string &name,
@@ -85,78 +264,70 @@ video::ITexture *add_texture_with_mipmaps(const std::string &name,
 	// see https://git.io/vNgmX
 	int mipmapcnt = MYMAX(ceil(logf(w) / logf(2)), ceil(logf(h) / logf(2)));
 
+	// ensure rgba size
+	if (img->getImageDataSizeInBytes() != w * h * 4)
+		errorstream << "size is " << img->getImageDataSizeInBytes() <<
+			" but expected " << w*h*4 << std::endl;
+	if (img->getColorFormat() != video::ECF_A8R8G8B8)
+		errorstream << "unexpected colour format" << std::endl;
+
 	// put the original texture into a matrix
-	assert(img->getImageDataSizeInBytes() == w * h * 4); // ensure rgba size
 	//~ u32 *raw = (u32 *)img->lock(video::ETLM_READ_ONLY, 0);
 	u32 *raw = (u32 *)img->lock();
 	struct matrix *matrices = image_to_matrices(raw, w, h);
 	img->unlock();
-	for (int i = 0; i < 4; ++i)
-		free(matrices[i].data);
-	free(matrices);
 
-
-
-
-
-	// total size of all mipmap images in bytes
-	u32 data_size = 0;
-	video::IImage *smallers[mipmapcnt];
-	// generate images
 	int k;
+	// get the total size of all mipmap images in bytes
+	int total_pixel_cnt = 0;
 	for (k = 0; k < mipmapcnt; ++k) {
-		if (dim.Width == 1 || dim.Height == 1)
+		w /= 2;
+		h /= 2;
+		if (h == 0)
+			h = 1;
+		else if (w == 0)
+			w = 1;
+		total_pixel_cnt += w * h;
+	}
+	u32 *data = new u32[total_pixel_cnt];
+
+	w = dim.Width;
+	h = dim.Height;
+
+	//~ video::IImage *current_image = (video::IImage *)data
+
+	// generate images
+	u32 *current_image = data;
+	for (k = 0; k < mipmapcnt; ++k) {
+		if (w == 1 || h == 1)
 			// stripes are downscaled differently (they usually don't appear)
 			break;
-		dim.Width /= 2;
-		dim.Height /= 2;
-		video::IImage *smaller = driver->createImage(img->getColorFormat(),
-			dim);
-		//~ sanity_check(smaller != NULL);
-		// custom scaling algorithm
-		video::SColor col = video::SColor(255, 255, 255, 200);
-		smaller->fill(col);
-		// get image size
-		data_size += smaller->getImageDataSizeInBytes();
-		smallers[k] = smaller;
+		w /= 2;
+		h /= 2;
+		// each step the size is halved and floored
+		int downscaling_factor = 1 << (k+1);
+		downscale_an_image(matrices, downscaling_factor, current_image);
+		// make current_image point to the next smaller image
+		current_image += w * h;
 	}
 	for (; k < mipmapcnt; ++k) {
 		// stripe downscaling, this only happens for non-square textures
-		dim.Width /= 2;
-		dim.Height /= 2;
-		if (!dim.Width)
-			dim.Width = 1;
-		else if (!dim.Height)
-			dim.Height = 1;
-		video::IImage *smaller = driver->createImage(img->getColorFormat(),
-			dim);
-		//~ sanity_check(smaller != NULL);
-		// custom scaling algorithm
-		video::SColor col = video::SColor(255, 255, 255, 200);
-		smaller->fill(col);
-		// get image size
-		data_size += smaller->getImageDataSizeInBytes();
-		smallers[k] = smaller;
-	}
-	// allocate memory for all image data
-	u8* data = new u8[data_size];
-	int current_pos = 0;
-	// copy image data to the array
-	for (int k = 0; k < mipmapcnt; ++k) {
-		u8* raw = (u8*) smallers[k]->lock();
-		u32 len = smallers[k]->getImageDataSizeInBytes();
-		memcpy(data + current_pos, raw, len);
-		smallers[k]->unlock();
-		current_pos += len;
+		w /= 2;
+		h /= 2;
+		if (w == 0)
+			w = 1;
+		else if (h == 0)
+			h = 1;
+		// not yet implemented
+		current_image += w * h;
 	}
 
 	// create the texture
 	video::ITexture *tex = driver->addTexture(name.c_str(), img, data);
 
 	// clean up memory
+	free_matrices(matrices);
 	delete[] data;
-	for (int k = 0; k < mipmapcnt; ++k)
-		smallers[k]->drop();
 
 	return tex;
 }
